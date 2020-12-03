270ae53f6cbc2c28383ffcf8a4499008dba6f1c5
Beatriz German <beatriz.german@kairosds.com>
2020-12-03 16:08:36 +0100

    add form

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+function App() {
+
+  const [task, setTask] = useState('')
+
+  function addTask(e) {
+    e.preventDefault() //para que en nuestro formulario no se procese el get
+    if(!task.trim()){
+      console.log('no has escrito nada');
+      return
+    } 
+    console.log(task);
+    setTask(''); 
+
+  }
+
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD SIMPLE</h1>
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Lista de tareas</h4>
+          <ul className="list-group">
+            <li className="list-group-item">
+              <span className="lead">Nombre de la tarea</span>
+              <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+              <button className="btn btn-warning btn-sm float-right">Editar</button>
+            </li>
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Formulario</h4>
+          <form onSubmit={e=>addTask(e)}>
+            <input 
+              type="text" 
+              className="form-control mb-2"
+              placeholder="Añade una tarea"
+              onChange= { e => setTask(e.target.value)} // estoy añadiendo al estado lo que hay en el input
+              value= {task}
+            />
+            <button className="btn btn-dark btn-block" type="submit"> Añadir</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
