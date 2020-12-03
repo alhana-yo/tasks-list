112bcfcdd1659fcc72f0000cadcea2d01d6d5b42
Beatriz German <beatriz.german@kairosds.com>
2020-12-03 16:39:03 +0100

    draw list items

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import { useState } from "react";
 
 function App() {
 
-  const [task, setTask] = useState('')
-
+  const [task, setTask] = useState('');
+  const [itemList, setItemList] = useState([]);
+  
+  
   function addTask(e) {
     e.preventDefault() //para que en nuestro formulario no se procese el get
     if(!task.trim()){
@@ -11,7 +13,13 @@ function App() {
       return
     } 
     console.log(task);
+    drawItemList(task);
     setTask(''); 
+
+  }
+
+  function drawItemList(task) {
+    setItemList([...itemList, {id:itemList.length, task}]);
 
   }
 
@@ -22,16 +30,24 @@ function App() {
         <div className="col-8">
           <h4 className="text-center">Lista de tareas</h4>
           <ul className="list-group">
-            <li className="list-group-item">
+            {/* <li className="list-group-item">
               <span className="lead">Nombre de la tarea</span>
               <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
               <button className="btn btn-warning btn-sm float-right">Editar</button>
-            </li>
+            </li> */}
+            {
+              itemList.map( item =>  <li className="list-group-item" key={item.id}>
+                                      <span className="lead">{ item.task }</span>
+                                      <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                                      <button className="btn btn-warning btn-sm float-right">Editar</button>
+                                    </li>
+                          )
+            }
           </ul>
         </div>
         <div className="col-4">
           <h4 className="text-center">Formulario</h4>
-          <form onSubmit={e=>addTask(e)}>
+          <form onSubmit={ e=>addTask(e) }>
             <input 
               type="text" 
               className="form-control mb-2"
